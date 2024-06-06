b6d41fd0f35726b71852c0452a279b855ad3e9ea
Igor Kyseľ <igor.kysel@centrum.sk>
2024-06-06 16:09:01 +0200

    Zmena formulácie počtu bodov

--- game.js
+++ game.js
@@ -7,8 +7,7 @@ async function populate() {
   const response = await fetch(request);
   countries = await response.json();
   //at the beginning, the button is disabled, we can enable it now that the json is loaded.
-  var button = document.getElementById("start");
-  button.disabled = false;
+  document.getElementById("start").disabled = false;
   //let's shuffle the list
   countries.sort(function(){return 0.5 - Math.random()});
 }
@@ -36,6 +35,7 @@ function getAnswer() {
   console.log(possibleScore)
   document.getElementById("score").innerHTML = score;
   document.getElementById("possibleScore").innerHTML = possibleScore;
+  document.getElementById("topScore").innerHTML = countries.lenght - possibleScore;
   nextQuestion++;
   game();
 }
